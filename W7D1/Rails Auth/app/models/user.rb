952005cc68fb3d6e5e36d03db5922af5d8d190af
zabala <zabalasteven9@gmail.com>
2023-03-13 09:12:57 -0700

    Working on helper methods

--- W7D1/Rails Auth/app/models/user.rb
+++ W7D1/Rails Auth/app/models/user.rb
@@ -1,7 +1,12 @@
+require 'securerandom'
+
 class User < ApplicationRecord
     def ensure_session_token
         if :session_token == nil
-            generate_unique_session_token
+            token = generate_unique_session_token
+            :session_token = token
+        else
+            true
         end
     end
 
@@ -9,16 +14,22 @@ class User < ApplicationRecord
     validates :session_token, presence:true
 
     def self.find_by_credentials(username, password)
-
+        if username == :username
+            return :username
+        else
+            return nil
+        end
     end
 
     def is_password?(password)
         if password != :password_digest
             puts "The password is incorrect. Please, try again."
+        else
+            true
         end
     end
 
     def generate_unique_session_token
-
+        SecureRandom.base64(16)
     end
 end
