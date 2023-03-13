10fad1fea7c0bdab8defbffb0bd053686fba777c
zabala <zabalasteven9@gmail.com>
2023-03-13 08:52:37 -0700

    Working on helper methods

--- W7D1/Rails Auth/app/models/user.rb
+++ W7D1/Rails Auth/app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ApplicationRecord
+    def ensure_session_token
+        if :session_token == nil
+            generate_unique_session_token
+        end
+    end
+
     validates :username, presence:true
     validates :session_token, presence:true
+
+    def self.find_by_credentials(username, password)
+
+    end
+
+    def is_password?(password)
+        if password != :password_digest
+            
+        end
+    end
+
+    def generate_unique_session_token
+
+    end
 end
